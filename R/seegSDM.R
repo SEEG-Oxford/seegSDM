97f4406d15f0619208ace28cf81bd8a07a7ca8e9
goldingn <goldingn35@gmail.com>
2013-12-09 11:51:00 +0000

    more comments in calcStats, edits to doc, edited auc2 to be a bit closer to the style guide

--- R/seegSDM.R
+++ R/seegSDM.R
@@ -20,29 +20,28 @@ auc2 <- function (DATA,
                   which.model = 1,
                   na.rm = FALSE) {
   if (is.logical(st.dev) == FALSE) {
-    stop("'st.dev' must be of logical type")
+    stop ("'st.dev' must be of logical type")
   }
   if (is.logical(na.rm) == FALSE) {
-    stop("'na.rm' must be of logical type")
+    stop ("'na.rm' must be of logical type")
   }
   if (sum(is.na(DATA)) > 0) {
     if (na.rm == TRUE) {
       NA.rows <- apply(is.na(DATA), 1, sum)
-      warning(length(NA.rows[NA.rows > 0]), " rows ignored due to NA values")
+      warning (length(NA.rows[NA.rows > 0]), " rows ignored due to NA values")
       DATA <- DATA[NA.rows == 0, ]
-    }
-    else {
-      return(NA)
+    } else {
+      return (NA)
     }
   }
   if (length(which.model) != 1) {
-    stop("this function will only work for a single model, 'which.model' must be of length one")
+    stop ("this function will only work for a single model, 'which.model' must be of length one")
   }
   if (which.model < 1 || round(which.model) != which.model) {
-    stop("'which.model' must be a positive integer")
+    stop ("'which.model' must be a positive integer")
   }
   if (which.model + 2 > ncol(DATA)) {
-    stop("'which.model' must not be greater than number of models in DATA")
+    stop ("'which.model' must not be greater than number of models in DATA")
   }
   DATA <- DATA[, c(1, 2, which.model + 2)]
   DATA[DATA[, 2] > 0, 2] <- 1
@@ -51,10 +50,9 @@ auc2 <- function (DATA,
   if (length(OBS[OBS == 1]) == 0 || length(OBS[OBS == 1]) == 
         nrow(DATA)) {
     if (st.dev == FALSE) {
-      return(NaN)
-    }
-    else {
-      return(data.frame(AUC = NaN, AUC.sd = NaN))
+      return (NaN)
+    } else {
+      return (data.frame(AUC = NaN, AUC.sd = NaN))
     }
   }
   rm(DATA)
@@ -66,44 +64,73 @@ auc2 <- function (DATA,
   R <- rank(PRED, ties.method = "average")
   R0 <- R[OBS == 0]
   R1 <- R[OBS == 1]
-  U <- n0 * n1 + (n0 * (n0 + 1))/2 - sum(R0)
-  AUC <- U/(n0 * n1)
+  U <- n0 * n1 + (n0 * (n0 + 1)) / 2 - sum(R0)
+  AUC <- U / (n0 * n1)
   
-  # this line turns terrible predictions.
-  # deleted because JESUS. CHRIST.
+  # This line turns terrible predictions into excellent ones.
+  # Deleted because JESUS. CHRIST.
   #   AUC[AUC < 0.5] <- 1 - AUC
   rm(PRED)
   rm(OBS)
   if (st.dev == FALSE) {
-    return(AUC = AUC)
-  }
-  else {
+    return (AUC = AUC)
+  } else {
     RR0 <- rank(PRED.0, ties.method = "average")
     RR1 <- rank(PRED.1, ties.method = "average")
-    pless.0 <- (R0 - RR0)/n1
-    pless.1 <- (R1 - RR1)/n0
+    pless.0 <- (R0 - RR0) / n1
+    pless.1 <- (R1 - RR1) / n0
     var.0 <- var(pless.0)
     var.1 <- var(pless.1)
-    var.AUC <- (var.0/n0) + (var.1/n1)
-    st.dev.AUC <- var.AUC^0.5
-    return(data.frame(AUC = AUC, AUC.sd = st.dev.AUC))
+    var.AUC <- (var.0 / n0) + (var.1 / n1)
+    st.dev.AUC <- var.AUC ^ 0.5
+    return (data.frame(AUC = AUC, AUC.sd = st.dev.AUC))
   }
 }
 
-# calculate range of validation statistics for fitted models
+# Calculate range of validation statistics for fitted models.
 # df is a dat.frame or matrix containing observed 0 or 1 data in
 # the first column and (0, 1] predictions in the second
 calcStats <- function(df) {
+  
+  # ad an id column (needed for PresenceAbsence functions)
   df <- data.frame(id = 1:nrow(df), df)
+  
+  # ~~~~~~~~~~~
+  # copntinuous probability metrics
+  
+  # bernoulli deviance
+  dev <- devBern(df[, 2], df[, 3])
+  
+  # root mean squared error
+  rmse <- rmse(df[, 2], df[, 3])
+
+  # auc (using my safe version - see above)
+  auc <- auc2(df, st.dev = TRUE)
+  
+  # ~~~~~~~~~~~  
+  # discrete classification metrics
+  
+  # calculate the 'optimum' threshold - one at which sensitivity == specificity
   opt <- optimal.thresholds(df, threshold = 101, which.model = 1, 
                             opt.methods = 3)
+  
+  # create confusiuon matrix at this threshold
   confusion <- cmx(df, threshold = opt[1, 2])
+
+  # kappa (using threshold)
   kappa <- Kappa(confusion, st.dev = TRUE)
-  auc <- seegSDM::auc2(df, st.dev = TRUE)
+  
+  # sensitivity and specificity using threshold
   sens <- sensitivity(confusion, st.dev = TRUE)
   spec <- specificity(confusion, st.dev = TRUE)
+  
+  # proportion correctly classified using threshold
   pcc <- pcc(confusion, st.dev = TRUE)
-  results <- c(kappa = kappa[, 1],
+  
+  # create results vector
+  results <- c(deviance = dev,
+               rmse = rmse,
+               kappa = kappa[, 1],
                auc = auc[, 1],
                sens = sens[, 1],
                spec = spec[, 1],
@@ -114,7 +141,9 @@ calcStats <- function(df) {
                spec_sd = spec[, 2],
                pcc_sd = pcc[, 2],
                thresh = opt[1, 2])
-  return(results)
+  
+  # and return it
+  return (results)
 }
 
 
